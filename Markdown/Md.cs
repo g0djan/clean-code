f4c8c762a2cd65e50643930ab2b5c4ef992961b2
godjan <g0dj4n@gmail.com>
2017-11-09 03:13:57 +0500

    architecture

--- Markdown/Md.cs
+++ Markdown/Md.cs
@@ -7,15 +7,100 @@ namespace Markdown
 {
 	public class Md
 	{
-		public string RenderToHtml(string markdown)
+	    private HashSet<Tag> tags;
+	    private TextTagsState state;
+	    private TagsFinder finder;
+	    private TagsReplacer replacer;
+
+	    public Md()
+	    {
+	        tags = new HashSet<Tag>();
+            state = new TextTagsState();
+            finder = new TagsFinder();
+            replacer = new TagsReplacer();
+        }
+
+        public string RenderToHtml(string markdown)
 		{
+            
 			return markdown; //TODO
 		}
 	}
 
+    public class TagsReplacer
+    {
+        private Dictionary<TagName, string> openedTagsReplacement;
+        private Dictionary<TagName, string> closedTagsReplacement;
+
+        public TagsReplacer()
+        {
+            openedTagsReplacement = new Dictionary<TagName, string>();
+            closedTagsReplacement = new Dictionary<TagName, string>();
+        }
+
+        public string ReplaceTags(string text, List<Tag> tags)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class TagsFinder
+    {
+        private Dictionary<string, string> closedTag;
+
+        public TagsFinder()
+        {
+            closedTag = new Dictionary<string, string>();
+        }
+
+        public Tag GetFirstTagOnSegment(string text, TextTagsState state, int startIndex, int endIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        private int GetIndexFirstOpenedTag(int startIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        private int GetIndexFirstClosedTag(int startIndex, TagName tagName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class TextTagsState
+    {
+        private BitArray mask;
+        private Dictionary<TagName, int> tagsNumbers;
+
+        public TextTagsState()
+        {
+            mask = new BitArray(2);
+            tagsNumbers = new Dictionary<TagName, int>();
+        }
+
+        public void SetInTag(TagName name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetOutTag(TagName name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsInTag(TagName name)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+
     public class Tag
     {
-        public readonly TagName name ;
+        public readonly TagName name;
         public readonly int openIndex;
         public readonly int closeIndex;
 
@@ -24,41 +109,6 @@ namespace Markdown
             this.openIndex = openIndex;
             this.closeIndex = closeIndex;
             this.name = name;
-        }
-    }
-
-    public class TagHandler
-    {
-        private readonly string text;
-        private Dictionary<TagName, string> openedTagsReplacement;
-        private Dictionary<TagName, string> closedTagsReplacement;
-        private Dictionary<string, string> closedTag;
-        private HashSet<Tag> tags;
-
-        public TagHandler(string text)
-        {
-            this.text = text;
-            tags = new HashSet<Tag>();
-        }
-
-        private void AddTagPairsOnSegment(int startIndex, int endIndex, BitArray tagsBitMask)
-        {
-            throw new NotImplementedException();
-        }
-
-        private int FindClosedTagPosition(int startIndex, int endIndex, TagName tagName, BitArray tagBitMask)
-        {
-            throw new NotImplementedException();
-        }
-
-        private Tag FindFirstTag(int startIndex, int endIndex)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string ReplaceTags()
-        {
-            throw new NotImplementedException();
         }
     }
 
